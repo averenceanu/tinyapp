28bd6b9bf2e0efd9b29a7e3774a2d1eff21a91c4
Alexandra Verenceanu <alexandra.verenceanu@mail.mcgill.ca>
2021-12-07 19:44:15 +0000

    Detele URL button implemented

--- express_server.js
+++ express_server.js
@@ -33,32 +33,43 @@ app.get("/hello", (req, res) => {
   res.send("<html><body>Hello <b>World</b></body></html>\n");
 });
 
+//Home Page 
 app.get("/urls", (req, res) => {
   const templateVars = { urls: urlDatabase }; 
   res.render('urls_index', templateVars);
 });
 
+//Create new URL
 app.get('/urls/new', (req, res) => {
   res.render('urls_new')
 });
 
+//When submit botton clicked > generate new shortURL > redirect to urls/newShortURL
 app.post("/urls", (req, res) => {
   let randomString = generateRandomString();
   urlDatabase[randomString] = req.body.longURL;
   res.redirect(`/urls/${randomString}`)
 });
 
+//Page with the newShortURL 
 app.get("/urls/:shortURL", (req, res) => {
   const shortURL = req.params.shortURL;
   const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[shortURL]};
   res.render("urls_show", templateVars);
 });
 
+//When clicked on newShortURL > redirects to longURL web page; 
 app.get("/u/:shortURL", (req, res) => {
   const shortURL = req.params.shortURL;
   const longURL = urlDatabase[shortURL];
   res.redirect(longURL);
 });
+
+//When delete URL 
+app.post("/urls/:shortURL/delete", (req, res) => {
+  delete urlDatabase[req.params.shortURL];
+  res.redirect("/urls");
+})
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
