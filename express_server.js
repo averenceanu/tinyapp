11940d8859b5b07289a88fc52ef457a9cf276a22
Alexandra Verenceanu <alexandra.verenceanu@mail.mcgill.ca>
2022-01-06 05:18:38 +0000

    Cookies cleaned after logout

--- express_server.js
+++ express_server.js
@@ -181,8 +181,9 @@ app.post("/login", (req, res) => {
 
 ////////////////////////////// LOGOUT //////////////////////////////
 app.post("/logout", (req, res) => {
-  req.session['user_id'] = null;
+  req.session = null;
   res.redirect("/urls");
+
 });
 
 ////////////////////////////// REDIRECT //////////////////////////////
